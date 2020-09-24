291f71be7c570d36fd6a8ade1a0c55484cfc369c
Kolawole Akinde <akinde.kolawole@gmail.com>
2020-09-24 21:01:57 +0100

    completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -39,4 +39,10 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view){
+       Intent intent=new Intent(Intent.ACTION_DIAL);
+       Uri uri=Uri.parse("tel:0123456789");
+       intent.setData(uri);
+       startActivity(intent);
+    }
 }
